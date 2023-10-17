d2d6810b4489259124da3f4332e0f740456bdc16
loyisoemma <loyisoemma@gmail.com>
2023-10-17 15:00:44 +0100

    change on 17th oct

--- resources/js/src/views/DataSample.vue
+++ resources/js/src/views/DataSample.vue
@@ -5,7 +5,7 @@
         <!-- First Column -->
         <div class="row mb-3 ">
           <div class="col min-vh-50 d-flex">
-            <div class="border p-3 flex-grow-1">Row 1, Column 1</div>
+            <div class="border p-3 flex-grow-1"> <svg id="num1"> </svg> </div>
           </div>
           <div class="col min-vh-50 d-flex">
             <div class="border p-3 flex-grow-1">Row 1, Column 2</div>
@@ -44,6 +44,7 @@
   } from 'bootstrap-vue';
   import StatisticCardHorizontal from "@core/components/statistics-cards/StatisticCardHorizontal.vue";
   import vSelect from 'vue-select'
+  import * as d3 from 'd3'
   
   export default {
     components: {
@@ -69,9 +70,87 @@
     },
 
     data() {
+
+      const chartdata = [
+    { date: "24-Apr-07", amount: 93.24 },
+    { date: "25-Apr-07", amount: 95.35 },
+    { date: "26-Apr-07", amount: 98.84 },
+    { date: "27-Apr-07", amount: 99.92 },
+    { date: "30-Apr-07", amount: 99.8 },
+    { date: "1-May-07", amount: 99.47 },
+    { date: "2-May-07", amount: 100.39 },
+    { date: "3-May-07", amount: 100.4 },
+    { date: "4-May-07", amount: 100.81 },
+    { date: "7-May-07", amount: 103.92 },
+    { date: "8-May-07", amount: 105.06 },
+    { date: "9-May-07", amount: 106.88 },
+    { date: "10-May-07", amount: 107.34 },
+  ];  
       return {  
-        
+        chartdata
       }
+    },
+
+    mounted(){
+        const width = 800;
+        const height = 500;
+        const svg = d3.select("#num1").attr("width", width).attr("height", height);
+        const g = svg.append("g");
+
+        //2. Parse the dates
+        const parseTime = d3.timeParse("%d-%b-%y");
+
+        //3. Creating the Chart Axes
+        const x = d3
+        .scaleTime()
+        .domain(
+            d3.extent(this.chartdata, function (d) {
+            return parseTime(d.date);
+            })
+        )
+        .rangeRound([0, width]);
+
+        const y = d3
+        .scaleLinear()
+        .domain(
+            d3.extent(this.chartdata, function (d) {
+            return d.amount;
+            })
+        )
+        .rangeRound([height, 0]);
+
+        //4. Creating a Line
+        const line = d3
+        .line()
+        .x(function (d) {
+            return x(parseTime(d.date));
+        })
+        .y(function (d) {
+            return y(d.amount);
+        });
+
+        //5. Appending the Axes to the Chart
+        g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+        g.append("g")
+        .call(d3.axisLeft(y))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Price ($)");    
+
+        //6. Appending a path to the Chart
+        g.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
     },
 
     setup() {   
