8bae231edf409de22781d20b4a0e44fda50af58a
loyisoemma <loyboy606@gmail.com>
2024-03-28 10:45:19 +0100

    delete jest config again

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    transform: {
-        // process `*.vue` files with `vue-jest` this is the most important thing to transform your VUE code!
-        "^.+\\.vue$": "vue-jest",
-        "^.+\\.js$": "babel-jest",
-    },
-}
