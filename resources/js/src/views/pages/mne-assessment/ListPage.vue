9f9f820dc8d507dffe7f87a1d693e5fab0d35337
loyboy <loyboy606@gmail.com>
2025-09-16 11:45:11 +0100

    changes small 16th sept 7

--- resources/js/src/views/pages/mne-assessment/ListPage.vue
+++ resources/js/src/views/pages/mne-assessment/ListPage.vue
@@ -277,6 +277,20 @@
                   </div>
                 </template>
 
+                <template #row="row">
+                  <tr v-if="row.item.performance === 0">
+                    <td :colspan="dynamicFields.length" class="text-center text-danger">
+                      No performance data available for
+                      {{ row.item.student_name || row.item.teacher_name || "Unknown" }}
+                    </td>
+                  </tr>
+                  <tr v-else>
+                    <td v-for="field in row.fields" :key="field.key">
+                      {{ row.item[field.key] }}
+                    </td>
+                  </tr>
+                </template>
+
                 <template #cell(performance)="data">
                    <b> {{ data.item.performance }} % </b>
                 </template>
