5cc7d1a25b6b76f05d19ef16d381cb6b6a927b7b
loyboy <loyboy606@gmail.com>
2025-09-22 16:18:45 +0100

    changes small 22nd 22

--- resources/js/src/views/pages/timetables/useTimetableList.js
+++ resources/js/src/views/pages/timetables/useTimetableList.js
@@ -1,139 +1,140 @@
 import { ref, watch, computed } from '@vue/composition-api'
 import store from '@/store'
- 
+
 // Notification
 import { useToast } from 'vue-toastification/composition'
 import ToastificationContent from '@core/components/toastification/ToastificationContent.vue'
 
 export default function useTimetableList(School = null, Teacher = null) {
-  // Use toast
-  const toast = useToast()
+    // Use toast
+    const toast = useToast()
 
-  const refTimetableListTable = ref(null)
+    const refTimetableListTable = ref(null)
 
-  // Table Handlers
-  const tableColumns = [
-    
-    { key: 'school.name', label: 'School Name', sortable: true },
-    { key: 'teacher.fname',label: 'Teacher Name',  sortable: true },
-    { key: 'class_stream.title',label: 'Class Name',  sortable: true },
-    { key: 'class_stream.ext',label: 'Class Arm',  sortable: true },
-    { key: 'subject.name',label: 'Subject Name',  sortable: true },
-    { key: 'time_of',label: 'Time of Class',  sortable: true },
-    { key: 'day_of',label: 'Day of Class',  sortable: true },
-    { key: 'status',label: 'Status',  sortable: true },
-    { key: 'actions' }
-  ]
-  const perPage = ref(10)
-  const totalTimetables = ref(0)
-  const currentPage = ref(1)
-  const perPageOptions = [10, 25, 50, 100]
-  const searchQuery = ref('')
-  const sortBy = ref('id')
-  const isSortDirDesc = ref(true)
-  const dayData = ref([ "Sunday", "Monday", "Tuesday" , "Wednesday" , "Thursday" , "Friday" ]);
-  const filters = ref({
-    schoolgroup: null,
-    schoolid: null,
-    teacherid: null,
-    supervisor: null
-  });
- 
-  const totalActiveTimetables = ref(0)
-  const totalInactiveTimetables = ref(0)
-  const totalDeletedTimetables = ref(0)
+    // Table Handlers
+    const tableColumns = [
 
-  const dataMeta = computed(() => {
-    const localItemsCount = refTimetableListTable.value ? refTimetableListTable.value.localItems.length : 0
+        { key: 'school.name', label: 'School Name', sortable: true },
+        { key: 'teacher.fname', label: 'Teacher Name', sortable: true },
+        { key: 'class_stream.title', label: 'Class Name', sortable: true },
+        { key: 'class_stream.ext', label: 'Class Arm', sortable: true },
+        { key: 'subject.name', label: 'Subject Name', sortable: true },
+        { key: 'time_of', label: 'Time of Class', sortable: true },
+        { key: 'day_of', label: 'Day of Class', sortable: true },
+        { key: 'status', label: 'Status', sortable: true },
+        { key: 'actions' }
+    ]
+    const perPage = ref(10)
+    const totalTimetables = ref(0)
+    const currentPage = ref(1)
+    const perPageOptions = [10, 25, 50, 100]
+    const searchQuery = ref('')
+    const sortBy = ref('id')
+    const isSortDirDesc = ref(true)
+    const dayData = ref(["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]);
+    const filters = ref({
+        schoolgroup: null,
+        schoolid: null,
+        teacherid: null,
+        supervisor: null,
+        admin: null
+    });
+
+    const totalActiveTimetables = ref(0)
+    const totalInactiveTimetables = ref(0)
+    const totalDeletedTimetables = ref(0)
+
+    const dataMeta = computed(() => {
+        const localItemsCount = refTimetableListTable.value ? refTimetableListTable.value.localItems.length : 0
+        return {
+            from: perPage.value * (currentPage.value - 1) + (localItemsCount ? 1 : 0),
+            to: perPage.value * (currentPage.value - 1) + localItemsCount,
+            of: totalTimetables.value,
+        } //
+    })
+
+    const refetchData = () => {
+        refTimetableListTable.value.refresh()
+    }
+
+    watch([currentPage, perPage, searchQuery], () => {
+        refetchData()
+    })
+
+    const fetchTimetables = (ctx, callback) => {
+        store
+            .dispatch('app-timetable/fetchTimetables', {
+                size: perPage.value,
+                page: currentPage.value - 1,
+                q: searchQuery.value,
+                schoolgroup: School || Teacher ? null : filters.value.schoolgroup,
+                school: School ? School : filters.value.schoolid,
+                teacher: Teacher ? Teacher : filters.value.teacherid,
+                supervisor: filters.value.admin ? filters.value.admin : filters.value.supervisor
+            })
+            .then(response => {
+                const { timetables, totalItems, totalActive, totalInactive, totalDeleted } = response.data
+
+                callback(timetables)
+                totalTimetables.value = totalItems
+                totalActiveTimetables.value = totalActive
+                totalInactiveTimetables.value = totalInactive
+                totalDeletedTimetables.value = totalDeleted
+
+            })
+            .catch((e) => {
+                console.log("Fetch Timetables error: " + e)
+                    /*toast({
+                      component: ToastificationContent,
+                      props: {
+                        title: 'Error fetching schools list',
+                        icon: 'AlertTriangleIcon',
+                        variant: 'danger',
+                      },
+                    })*/
+            })
+    }
+
+    const handlePageChange = (value) => {
+        currentPage.value = value;
+        fetchTimetables();
+    }
+
+    // *===============================================---*
+    // *--------- UI ---------------------------------------*
+    // *===============================================---*
+
+    const resolveTimetableStatusVariant = status => {
+        if (status === 0) return 'warning'
+        if (status === -1) return 'danger'
+        if (status === 1) return 'success'
+        return 'primary'
+    }
+
     return {
-      from: perPage.value * (currentPage.value - 1) + (localItemsCount ? 1 : 0),
-      to: perPage.value * (currentPage.value - 1) + localItemsCount,
-      of: totalTimetables.value,
-    }//
-  })
+        fetchTimetables,
+        handlePageChange,
 
-  const refetchData = () => {
-    refTimetableListTable.value.refresh()
-  }
+        totalTimetables,
+        totalActiveTimetables,
+        totalInactiveTimetables,
 
-  watch([currentPage, perPage, searchQuery], () => {
-    refetchData()
-  })
+        dayData,
 
-  const fetchTimetables = (ctx, callback) => {
-    store
-      .dispatch('app-timetable/fetchTimetables', {
-        size: perPage.value,
-        page: currentPage.value - 1,
-        q: searchQuery.value,
-        schoolgroup: School || Teacher ? null: filters.value.schoolgroup,
-        school: School ? School : filters.value.schoolid,
-        teacher: Teacher ? Teacher : filters.value.teacherid,
-        supervisor: filters.value.supervisor
-      })
-      .then(response => {
-        const { timetables, totalItems, totalActive, totalInactive, totalDeleted } = response.data
+        tableColumns,
+        perPage,
+        currentPage,
+        dataMeta,
+        perPageOptions,
+        searchQuery,
+        sortBy,
+        isSortDirDesc,
 
-        callback(timetables)
-        totalTimetables.value = totalItems
-        totalActiveTimetables.value = totalActive
-        totalInactiveTimetables.value = totalInactive
-        totalDeletedTimetables.value = totalDeleted
+        refTimetableListTable,
+        filters,
 
-      })
-      .catch((e) => {
-        console.log("Fetch Timetables error: " + e)
-        /*toast({
-          component: ToastificationContent,
-          props: {
-            title: 'Error fetching schools list',
-            icon: 'AlertTriangleIcon',
-            variant: 'danger',
-          },
-        })*/
-      })
-  }
+        resolveTimetableStatusVariant,
+        refetchData
 
-  const handlePageChange = (value) => {
-    currentPage.value = value;
-    fetchTimetables();
-  }
-
-  // *===============================================---*
-  // *--------- UI ---------------------------------------*
-  // *===============================================---*
-
-  const resolveTimetableStatusVariant = status => {
-    if (status === 0) return 'warning'
-if (status === -1) return 'danger'
-    if (status === 1) return 'success'
-    return 'primary'
-  }
-
-  return {
-    fetchTimetables,
-    handlePageChange,
-
-    totalTimetables,
-    totalActiveTimetables,    
-    totalInactiveTimetables,
-
-    dayData,
-
-    tableColumns,
-    perPage,
-    currentPage,
-    dataMeta,
-    perPageOptions,
-    searchQuery,
-    sortBy,
-    isSortDirDesc,
-
-    refTimetableListTable,
-    filters,
-
-    resolveTimetableStatusVariant,
-    refetchData
-
-  }
+    }
 }
